4a226c3d8101fd93f3d1d09b26dfe3f0a495745d
3xp10it <quanyechavshuo@gmail.com>
2016-11-27 14:44:39 +0800

    update

--- uninstall.py
+++ uninstall.py
@@ -4,4 +4,5 @@ os.system("rm %sconfig.ini" % ModulePath)
 os.system("rm -r %slog" % ModulePath)
 os.system("pip3 uninstall exp10it")
 modulePath = os.path.abspath(__file__)[:-len(__file__.split("/")[-1])]
+os.chdir("/root/")
 os.system("rm -r %s" % modulePath) 
